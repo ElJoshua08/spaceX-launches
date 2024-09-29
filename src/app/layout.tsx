97d4729ab5eda05f6b2371645643350f0c3c4b0c
Joshua <josuealejandrof926@gmail.com>
2024-09-29 23:15:06 +0200

    Theme toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { ThemeProvider } from '@/components/theme-provider';
+
+import './globals.css';
+import { ThemeToggle } from '@/components/theme-toggle';
 
 export const metadata: Metadata = {
-  title: "SpaceX Launches",
-  description: "View all detailed information about SpaceX launches",
+  title: 'SpaceX Launches',
+  description: 'View all detailed information about SpaceX launches',
 };
 
 export default function RootLayout({
@@ -13,10 +16,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`antialiased`}
-      >
-        {children}
+      <body className={`antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="dark:hidden absolute top-0 -z-10 h-full w-full bg-white">
+            <div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]"></div>
+          </div>
+          <div className="hidden dark:block absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]" />
+          {children}
+          <div className="absolute bottom-4 left-4">
+            <ThemeToggle />
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );
